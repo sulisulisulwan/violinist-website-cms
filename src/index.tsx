a7772bd8d8589fc8d6d193c0b5d69a140f8431ef
Suliman Tekalli <sulimantekalli@sulimans-mbp.lan>
2024-04-02 19:40:37 -0400

    Add index.html and main.css to build steps in webpack and new production mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import App from './App'
 import * as ReactDom from 'react-dom/client'
-
+import "./main.css"
 
 const container = document.getElementById('app')
 const root = ReactDom.createRoot(container)
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,58 @@
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-import.meta
+export default (env) => {
 
-export default {
-  mode: 'development',
-  entry: __dirname + '/src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+  return {
+    mode: env.MODE,
+    entry: __dirname + '/src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
           }
-        }
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-      },
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: path.resolve(__dirname, "./dist/"),
-  
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".tsx", ".ts"]
-  },
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename:"main.css"})
+    ],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      publicPath: path.resolve(__dirname, "./dist/"),
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".tsx", ".ts"]
+    },
+  }
 }
+
 
 
 
