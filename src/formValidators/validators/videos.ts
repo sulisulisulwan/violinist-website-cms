6e7ea241e3737942f6cb528324094be5ac01c150
Suliman Tekalli <sulimantekalli@sulimans-mbp.lan>
2024-12-06 23:56:00 -0500

    Fix validators and add production build command

--- src/formValidators/validators/videos.ts
+++ src/formValidators/validators/videos.ts
@@ -5,11 +5,25 @@ export const videosFormValidator = (formValues: videosFormFieldStateIF) => {
 
   const validationData = getValidationObject()
 
-  if (!formValues.youtubeCode.isValid) {
+  if (formValues.thumbnailUploadPath.currPref === 'defaultYT' && !formValues.youtubeCode.isValid) {
     validationData.isValid = false
     validationData.errorMessage = 'You must provide a valid youtube code!'
     return validationData
   }
+    
+  if (formValues.thumbnailUploadPath.currPref === 'custom') {
+    if (!formValues.thumbnailUploadPath.custom.length) {
+      validationData.isValid = false
+      validationData.errorMessage = 'If choosing a custom image, you must upload a file!'
+    }
+  } 
+
+  if (formValues.thumbnailUploadPath === null || formValues.thumbnailUploadPath.currPref === '') {
+    validationData.isValid = false
+    validationData.errorMessage = 'You must choose a thumbnail preference!'
+    return validationData
+  }
+
   
   if (!formValues.caption.length) {
     validationData.isValid = false
@@ -17,18 +31,6 @@ export const videosFormValidator = (formValues: videosFormFieldStateIF) => {
     return validationData
   }
 
-  if (formValues.thumbnailUploadPath === null) {
-    validationData.isValid = false
-    validationData.errorMessage = 'You must choose a thumbnail preference!'
-    return validationData
-  }
-  
-  if (formValues.thumbnailUploadPath.currPref === 'custom') {
-    if (!formValues.thumbnailUploadPath.custom.length) {
-      validationData.isValid = false
-      validationData.errorMessage = 'If choosing a custom image, you must upload a file!'
-    }
-  } 
 
   return validationData
 }
